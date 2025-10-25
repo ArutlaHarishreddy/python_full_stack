3710cda813ec9f6f716a4be4cf1718be384a8f0b
Harish Reddy <harishreddyr514@gmail.com>
2025-10-25 19:07:18 +0530

    python codes

---
+++ Interview Questions/File - 3/recursion_fibonacci.py
@@ -0,0 +1,15 @@
+#18. Implement a recursive function to compute Fibonacci numbers.
+def fibonacci(n):
+    if n<=0:
+        return "Invalid Input"
+    elif n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+n=int(input("Enter the number"))
+print(fibonacci(n))
+
+
+#recursion fibonacci
