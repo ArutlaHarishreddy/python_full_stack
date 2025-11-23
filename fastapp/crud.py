8295d72482b39f6e02f575fcaa3cafb037c8fb9a
Harish Reddy <harishreddyr514@gmail.com>
2025-11-23 14:47:35 +0530

    Fastapi codes

--- fastapp/crud.py
+++ fastapp/crud.py
@@ -1,5 +1,56 @@
+# from fastapi import FastAPI, HTTPException
+# from pydantic import BaseModel, Field
+
+# app=FastAPI()
+
+# user_details={}
+# count_id=1
+
+# class User(BaseModel):
+#     name:str = Field(..., min_length=3, max_length=20)
+#     phone:int
+#     age:int
+
+# @app.post("/create")
+# def create_user(user:User):
+#     global count_id
+#     user_details[count_id]=user
+#     response={
+#         "message":"User successfully created",
+#         "user_id":count_id,
+#         "user":user_details[count_id]
+#     }
+#     count_id += 1
+#     return response
+
+# @app.get("/users")
+# def get_users():
+#     if not user_details:
+#         raise HTTPException(status_code=404, detail="No users Found")
+#     return user_details
+
+# @app.get("/user/{user_id}")
+# def get_single_user(user_id:int):
+#     if user_id not in user_details:
+#         raise HTTPException(status_code=404, detail="No users Found")
+#     return user_details[user_id]
+
+# @app.put("/user/{user_id}")
+# def update_user(user_id:int,user:User):
+#     if user_id not in user_details:
+#         raise HTTPException(status_code = 404, detail="No user Found")
+#     user_details[user_id]=user
+#     return {"message":"user updated","user":user}
+
+# @app.delete("/user/{user_id}")
+# def delete_user(user_id:int):
+#     if user_id not in user_details:
+#         raise HTTPException(status_code=404,detail="No User Found")
+#     delete=user_details.pop(user_id)
+#     return {"message":"deleted successfully","user":delete}
+
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, Field
+from pydantic import BaseModel,Field
 
 app=FastAPI()
 
@@ -7,45 +58,44 @@ user_details={}
 count_id=1
 
 class User(BaseModel):
-    name:str = Field(..., min_length=3, max_length=20)
+    name:str=Field(..., min_length=5, max_length=20)
     phone:int
     age:int
 
 @app.post("/create")
-def create_user(user:User):
+def creating_users(user:User):
     global count_id
-    user_details[count_id]=user
+    user=user_details[count_id]
     response={
         "message":"User successfully created",
         "user_id":count_id,
         "user":user_details[count_id]
     }
-    count_id += 1
+    count_id+=1
     return response
-
 @app.get("/users")
-def get_users():
+def get_all_users():
     if not user_details:
-        raise HTTPException(status_code=404, detail="No users Found")
+        raise HTTPException(status_code=404,detail="Details not found")
     return user_details
 
 @app.get("/user/{user_id}")
 def get_single_user(user_id:int):
     if user_id not in user_details:
-        raise HTTPException(status_code=404, detail="No users Found")
+        raise HTTPException(status_code=404,detail="User not founds")
     return user_details[user_id]
 
 @app.put("/user/{user_id}")
 def update_user(user_id:int,user:User):
     if user_id not in user_details:
-        raise HTTPException(status_code = 404, detail="No user Found")
-    user_details[user_id]=user
-    return {"message":"user updated","user":user}
+        raise HTTPException(status_code=404,details="User not found")
+    user=user_details[user_id]
+    return {"message":"user updated successfully","user":user}
 
 @app.delete("/user/{user_id}")
 def delete_user(user_id:int):
     if user_id not in user_details:
-        raise HTTPException(status_code=404,detail="No User Found")
+        raise HTTPException(status_code=404, details="User not found")
     delete=user_details.pop(user_id)
-    return {"message":"deleted successfully","user":delete}
+    return {"Message":"User deleted successfully","user":delete}
 
