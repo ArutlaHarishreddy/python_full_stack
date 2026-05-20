c5fcb46e024254f31d373da9ef7add852968805b
Harish Reddy <harishreddyr514@gmail.com>
2026-05-20 09:17:11 +0530

    pushing the python practising codes

--- basic python codes/python practicing.py
+++ basic python codes/python practicing.py
@@ -86,12 +86,12 @@
 #         print(num)
 
 #printing prime count
-n=int(input("Enter any number:"))
-count=0
-for num in range(2,n+1):
-    for i in range(2,num):
-        if num%i==0:
-            break
-    else:
-        count+=1
-print(n,":",count)
+# n=int(input("Enter any number:"))
+# count=0
+# for num in range(2,n+1):
+#     for i in range(2,num):
+#         if num%i==0:
+#             break
+#     else:
+#         count+=1
+# print(n,":",count)
